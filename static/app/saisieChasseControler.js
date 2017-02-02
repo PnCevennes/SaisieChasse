14cd0a8bf0e59c7b0e1af400bc53861a43989e01
Amandine SAHL <amandine.sahl@gmail.com>
2017-02-02 17:13:59 +0100

    passage à npm et maj des librairies

--- static/app/saisieChasseControler.js
+++ static/app/saisieChasseControler.js
@@ -10,76 +10,73 @@ app.controller('listeBraceletCtrl',
 
   //@TODO : mettre en paramètre la saison de chasse
 	$http.get("/api/plan_chasse/bracelets_list/4")
-		.success(function(data, status, headers, config) {
-        	$scope.listBracelets = data
+		.then(function(response) {
+        	$scope.listBracelets = response.data
     	})
-    	.error(function(data, status, headers, config) {
-        	$scope.errors.push(data.message);
+    	.catch( function(response) {
+        	$scope.errors.push(response.data.message);
     	});
 
     $http.get('/api/lieux/communes')
-	    .success(function(data, status, headers, config) {
-	    	data.push({"id":-1, "value":"INCONNU"});
-	    	$scope.listCommunes = data;
+	    .then(function(response) {
+	    	response.data.push({"id":-1, "value":"INCONNU"});
+	    	$scope.listCommunes = response.data;
 		})
-		.error(function(data, status, headers, config) {
-			$scope.errors.push(data.message);
+		.catch( function(response) {
+			$scope.errors.push(response.data.message);
 		});
 
     $http.get('/api/plan_chasse/auteurs')
-	    .success(function(data, status, headers, config) {
-	    	$scope.listAuteurs = data
+	    .then(function(response) {
+	    	$scope.listAuteurs = response.data
 		})
-		.error(function(data, status, headers, config) {
-			$scope.errors.push(data.message);
+		.catch( function(response) {
+			$scope.errors.push(response.data.message);
 		});
 
     $http.get('/api/thesaurus/vocabulary/4?ilikeHierachie=004.00%25.%25')
-	    .success(function(data, status, headers, config) {
-	    	$scope.listZi = data;
+	    .then(function(response) {
+	    	$scope.listZi = response.data;
 	    	angular.forEach($scope.listZi, function(value, key) {
 			 $scope.listZi[key].code = parseInt(value.code);
 			});
 		})
-		.error(function(data, status, headers, config) {
-			$scope.errors.push(data.message);
+		.catch( function(response) {
+			$scope.errors.push(response.data.message);
 		});
+  $scope.onSelectBracelet = function ($item, $model, $label) {
+    $http.get("/api/plan_chasse/bracelet/"+$item.id)
+    .then(function(response) {
+      data = response.data
+      $scope.selectedCommune=undefined;
+      $scope.selectedLieuDit=undefined;
+          $scope.currentBracelet = data;
+          if ($scope.currentBracelet.date_exacte) $scope.currentBracelet.date_exacte = new Date($filter('date')(data.date_exacte, 'yyyy-MM-dd'));
+          if (data.cd_com) {
+            $scope.selectedCommune = $filter('filter')($scope.listCommunes, {id:data.cd_com})[0];
+          }
+          if (data.pk_nom_lieudit) {
+            $http.get('/api/lieux/'+data.pk_nom_lieudit)
+        .then(function(response) {
+            $scope.selectedLieuDit =response.data[0];
+        }).catch(function(response) {
+          $scope.errors.push(response.data.message);});
+          }
+      })
+      .catch(function(response) {
+          $scope.errors.push(response.data.message);
+      });
+  };
 
-	$scope.$watch('selectedBracelet',function (newValue, oldValue){
-		if (newValue) {
-			$http.get("/api/plan_chasse/bracelet/"+newValue.id)
-			.success(function(data, status, headers, config) {
-				$scope.selectedCommune=undefined;
-				$scope.selectedLieuDit=undefined;
-	        	$scope.currentBracelet = data;
-	        	if ($scope.currentBracelet.date_exacte) $scope.currentBracelet.date_exacte = new Date($filter('date')(data.date_exacte, 'yyyy-MM-dd'));
-	        	if (data.cd_com) {
-	        		$scope.selectedCommune = $filter('filter')($scope.listCommunes, {id:data.cd_com})[0];
-	        	}
-	        	if (data.pk_nom_lieudit) {
-	        		$http.get('/api/lieux/'+data.pk_nom_lieudit)
-					.success(function(data, status, headers, config) {
-					    $scope.selectedLieuDit =data[0];
-					}).error(function(data, status, headers, config) { $scope.errors.push(data.message);});
-	        	}
+  $scope.onSelectCommune = function ($item, $model, $label) {
+	   $http.get("/api/lieux/?code_com="+$item.id)
+			.then(function(response) {
+	        	$scope.listLieuDits = response.data;
 	    	})
-	    	.error(function(data, status, headers, config) {
-	        	$scope.errors.push(data.message);
+	    	.catch( function(response) {
+	        	$scope.errors.push(response.data.message);
 	    	});
-		}
-	});
-
-	$scope.$watch('selectedCommune', function(newValue, oldValue){
-		if (newValue) {
-			$http.get("/api/lieux/?code_com="+newValue.id)
-			.success(function(data, status, headers, config) {
-	        	$scope.listLieuDits = data;
-	    	})
-	    	.error(function(data, status, headers, config) {
-	        	$scope.errors.push(data.message);
-	    	});
-		}
-	});
+	};
 
 	$scope.update = function() {
 		 //if form is not valid then return the form.
@@ -94,13 +91,13 @@ app.controller('listeBraceletCtrl',
     currentData.auteur_constat = [].concat(currentData.auteur_constat).join();
     currentData.auteur_tir = [].concat(currentData.auteur_tir).join();
 		$http.post('/api/plan_chasse/bracelet/'+currentData.id, currentData)
-	        .success(function(data) {
-	            alert(data.message);
+	        .then(function(response) {
+	            alert(response.data.message);
 	            $scope.currentBracelet=undefined;
 	            $scope.selectedBracelet=undefined;
 	        })
-	        .error(function(data,status,error,config) {
-	            alert("Error: "+error+" Post to server failed.\nStatus: "+status);
+	        .catch( function(response) {
+	            alert("Error: "+response.error+" Post to server failed.\nStatus: "+response.status);
 	        });
 	};
 
@@ -135,11 +132,11 @@ app.directive('thesaurus', ['$http', function ($http) {
 			var query = $scope.idtype;
 		    if ($scope.queryParam) query += '?'+$scope.queryParam
 			$http.get("/api/thesaurus/vocabulary/"+query)
-				.success(function(data, status, headers, config) {
-		        	$scope.list = data
+				.then(function(response) {
+		        	$scope.list = response.data
 		    	})
-		    	.error(function(data, status, headers, config) {
-		        	console.log(data);
+		    	.catch( function(response) {
+		        	console.log(response);
 		    	});
 		},
     };
