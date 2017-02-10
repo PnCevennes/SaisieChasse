c1ef9b50c140de46a3e15243c1756e209848dcb7
Amandine SAHL <amandine.sahl@gmail.com>
2017-02-10 17:41:01 +0100

    ajout de la fonction authentification

--- server.py
+++ server.py
@@ -19,6 +19,10 @@ def init_app():
     from modules.index import routes
     app.register_blueprint(routes, url_prefix='/')
 
+
+    fnauth = importlib.import_module("modules.UsersHub-authentification-module.routes")
+    app.register_blueprint(fnauth.routes, url_prefix='/api/auth')
+
     from modules.chasse.routes import ltroutes, pcroutes
     app.register_blueprint(ltroutes, url_prefix='/api/lieux')
     app.register_blueprint(pcroutes, url_prefix='/api/plan_chasse')
--- static/app/app.js
+++ static/app/app.js
@@ -1,12 +1,50 @@
 var app = angular.module(
     'saisieChasseApp',
-    ['ngRoute','ui.bootstrap']
+    ['ngRoute','ui.bootstrap','ngCookies','toaster']
 );
+app.service('locationHistoryService', function(){
+    return {
+        previousLocation: null,
 
+        store: function(location){
+            //@TODO COMPRENDRE
+            this.previousLocation = location.replace('#!/', '');
+        },
+
+        get: function(){
+            return this.previousLocation;
+        }
+      }
+})
+.run(['$rootScope', '$location', 'locationHistoryService','loginSrv','toaster',
+  function($rootScope, $location, locationHistoryService,loginSrv, toaster){
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+      //Stockage de la dernière route
+      if (current) {
+        locationHistoryService.store(location.hash);
+      }
+      else{
+        current = '/';
+      }
+      if (!next.access) return;
+
+      if (next.access.restricted) {
+        (next.access.level === undefined) ? level = 0 : level= next.access.level;
+        if ((loginSrv.getToken() !== undefined) && (level <= loginSrv.getCurrentUser().id_droit_max)) return;
+        toaster.pop('error', 'Vous devez être identifié et avoir un niveau de droit suffisant', '', 2000, 'trustedHtml');
+        $location.path(current);
+      }
+    });
+}]);
 app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
-            .when('/', {
+            .when('/login', {
+                templateUrl: 'static/app/login/form.html',
+                controller: 'loginFormCtrl',
+                controllerAs: 'ctrl'
+            })
+            .when('/saisie_chasse', {
                 templateUrl: 'static/app/saisie_chasse/saisieChasse-tpl.html',
                 controller: 'listeBraceletCtrl'
             }).
