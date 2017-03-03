400a2ef8d6df8d51e14b349ff83e93cb913eb3ea
Amandine SAHL <amandine.sahl@gmail.com>
2017-03-03 17:01:31 +0100

    Ajout de la procedure installation

---
+++ install_app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. settings.ini
+
+echo "Création du fichier de configuration ..."
+cp config.py.sample config.py
+
+echo "préparation du fichier config.py..."
+#monuser:monpassachanger@localhost/taxhubdb
+sed -i "s/SQLALCHEMY_DATABASE_URI = .*$/SQLALCHEMY_DATABASE_URI = \"postgresql:\/\/$user_pg:$user_pg_pass@$db_host:$db_port\/$db_name\"/" config.py
+
+#installation des librairies
+cd static/
+npm intall
+cd ..
+
+#Installation du virtual env
+echo "Installation du virtual env..."
+virtualenv venv
+virtualenv -p sr/bin/python2.7 venv #TODO adapater le chemin à la version de python du server
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+# #création d'un fichier de configuration
+# cp static/app/constants.js.sample static/app/constants.js
+#
+# #affectation des droits sur le répertoire static/medias
+# chmod 775 static/medias
